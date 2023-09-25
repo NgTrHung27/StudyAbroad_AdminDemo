d818f483c9ef77165906080dbd4e457a7596facf
felixquang <tranquang071203@gmail.com>
2023-09-25 15:46:04 +0700

    user 2 pages

--- app/(home)/(routes)/layout.tsx
+++ app/(home)/(routes)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <div className="fixed w-full inset-y-0 z-50 h-[80px]">
-        <NavBtSeo />
+        <Navbar />
       </div>
       {children}
     </div>
---
+++ app/(home)/(routesTruong)/Truong1/page.jsx
@@ -0,0 +1,15 @@
+  import React from 'react';
+  import Image from 'next/image';
+  import { Welcome } from '../../../../components/users/welcome';
+  import { History } from '../../../../components/users/history';
+
+  function page() {
+    return (
+      <div className='absolute' >
+       <Welcome/>
+       <History/>
+      </div>
+    );
+  }
+
+  export default page;
