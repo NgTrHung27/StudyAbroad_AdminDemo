34f8ba1fadbfaf97dac97b88cdbdaed04111e70e
TreNgheDiCode <gabayan170@gmail.com>
2023-09-27 20:54:20 +0700

    User Data Table (Demo)

---
+++ app/(root)/(routes)/taikhoan/_components/columns.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { User, UserRole } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "username",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Tên tài khoản
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "role",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Vai trò
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const isAdmin = row.getValue("role") === UserRole.ADMIN || false;
+
+      return (
+        <Badge className={cn("bg-slate-500", isAdmin && "bg-rose-500")}>
+          {isAdmin ? "Quản trị viên" : "Người dùng"}
+        </Badge>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="h-4 w-8 p-0">
+              <span className="sr-only">Tùy chọn</span>
+              <MoreHorizontal className="h4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" />
+                Cập nhật
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- app/(root)/(routes)/taikhoan/page.tsx
+++ app/(root)/(routes)/taikhoan/page.tsx
@@ -1,5 +1,27 @@
-const TaiKhoanPage = () => {
-  return <div>Hello tài khoản</div>;
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
+import getCurrentUser from "@/actions/get-current-user";
+
+const TaiKhoanPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    redirect("/sign-in");
+  }
+
+  const users = await db.user.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={users} />
+    </div>
+  );
 };
 
 export default TaiKhoanPage;
