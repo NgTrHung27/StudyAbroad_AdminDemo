f0a3ccc78c68b37ed69ba0cd0f8c5dced1a1cfc6
PhuCuong2512 <131945083+PhuCuong2512@users.noreply.github.com>
2023-10-06 19:45:30 +0700

    id truong

--- app/(home)/(routesTruong)/[idTruong]/page.jsx
+++ app/(home)/(routesTruong)/[idTruong]/page.jsx
@@ -4,12 +4,9 @@ import { History } from "../../../../components/users/history";
 import Navbar from "../../../../components/navbar/navbar";
 import { Operation } from "../../../../components/users/operation";
 import { Blogs } from "../../../../components/users/blogs";
-<<<<<<< HEAD
 import Procedure from "../../../../components/users/procedure";
 import { Connect } from "../../../../components/users/connect";
 
-=======
->>>>>>> 8e40d0ef2bba578cd5b7e7dc5a58ba11c1e9d888
 function page({ params }) {
   return (
     <div>
