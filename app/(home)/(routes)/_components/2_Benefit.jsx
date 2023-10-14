6843c2a1e74a593dc9f571c84ba95ed4f34410a5
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2023-10-14 18:35:09 +0700

    Footer + Home Final

--- app/(home)/(routes)/_components/2_Benefit.jsx
+++ app/(home)/(routes)/_components/2_Benefit.jsx
@@ -1,11 +1,13 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import IconBenefit from "../_components/7_Icon_Benefit";
 
 const Benefit = () => {
   return (
-    <div className="text-[#7D1F1F]  font-bold text-center bg-gray-100">
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-2 p-8">
+    <div className="text-[#7D1F1F]  font-bold text-center bg-gray-100 md:px-24">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 pt-5 md:pt-8 ">
         <IconBenefit
           src="/IconSchool.png"
           description="Hơn 500 học sinh du học mỗi năm"
@@ -15,7 +17,9 @@ const Benefit = () => {
           description="Học bổng trung bình 100,000$"
         />
         <IconBenefit src="/IconNha.png" description="Nhiều chỗ ở tiện nghi" />
-        <IconBenefit
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 pt-3 md:pt-8 md:mx-52 md:mt-[-50px]">
+      <IconBenefit
           src="/IconJob.png"
           description="Gặp gỡ và giao lưu bạn bè trên thế giới"
         />
--- app/(home)/(routes)/layout.tsx
+++ app/(home)/(routes)/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="">
+    <div className="h-full">
       <div className="fixed w-full h-[80px] inset-y-0 z-10 shadow-sm">
         <NavBtSeo />
       </div>
