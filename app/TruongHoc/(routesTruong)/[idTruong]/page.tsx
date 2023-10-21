eeb32e44137011a9bb8186b6565fbd7b510618bb
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2023-10-21 16:20:24 +0700

    Nav Update + Chuyen cac trang

--- app/TruongHoc/(routesTruong)/[idTruong]/page.tsx
+++ app/TruongHoc/(routesTruong)/[idTruong]/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Welcome } from "../../../../../components/users/welcome";
-import { History } from "../../../../../components/users/history";
-import { OperationTop } from "../../../../../components/users/operation";
-import { OperationBody } from "../../../../../components/users/operationbody";
-import Procedure from "../../../../../components/users/procedure";
-import { Connect } from "../../../../../components/users/connect";
-import { Blogs } from "../../../../../components/users/blogs";
+import { Welcome } from "../../../../components/users/welcome";
+import { History } from "../../../../components/users/history";
+import { OperationTop } from "../../../../components/users/operation";
+import { OperationBody } from "../../../../components/users/operationbody";
+import Procedure from "../../../../components/users/procedure";
+import { Connect } from "../../../../components/users/connect";
+import { Blogs } from "../../../../components/users/blogs";
+
 
 function page() {
   return (
--- app/TruongHoc/(routesTruong)/layout.tsx
+++ app/TruongHoc/(routesTruong)/layout.tsx
@@ -1,11 +1,11 @@
-import FooterDemo from "../../../../components/Footer/footer";
-import Navbar from "../../../../components/navbar/navbar";
+import FooterDemo from "../../../components/Footer/footer";
+import Navbar from "../../../components/navbar/navbar";
 
 const AuthLayout = ({ children: children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex flex-col">
       <div className="flex-1">
-        <Navbar />
+        <Navbar  />
         <main className="h-full w-full pt-[72px]">{children}</main>
         <FooterDemo />
       </div>
