1027dbe020ebab7179c7a50fc69006cb006c28d4
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-10-23 16:37:55 +0700

    Merge branch 'master' of https://github.com/NgTrHung27/QlyDuHoc into huong

---
+++ app/(SoSanhTruong)/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./components/header";
+import { Body } from "./components/body";
+function Page() {
+  return (
+    <>
+     <Body/>
+      <Header />
+     
+    </>
+  );
+}
+export default Page;
