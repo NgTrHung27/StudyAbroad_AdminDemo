2486ba13e5717962aade408f7ed0df38c4e48b38
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2023-10-24 23:23:52 +0700

    fix loi B

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",B
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
