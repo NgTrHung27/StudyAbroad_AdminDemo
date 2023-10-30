46ab805d60677fd361caf4362fc8c28788b3e46f
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-10-30 17:29:39 +0700

    huong code ss

--- app/(Score)/layout.tsx
+++ app/(Score)/layout.tsx
@@ -1,19 +1,24 @@
-'use client'
 
 import Navbar from "../../components/navbar/navbar";
 import FooterDemo from "../../components/Footer/footer";
 import { NextUIProvider } from "@nextui-org/react";
+import prismadb from "../../lib/prismadb";
 
-const LienHeLayout = ({
+const LienHeLayout = async ({
   children: children,
 }: {
   children: React.ReactNode;
 }) => {
+  const schools = await prismadb.school.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <NextUIProvider>
       <div className="h-full flex flex-col">
         <div className="flex-1">
-          <Navbar />
+          <Navbar schools={schools} />
           <main className="w-full pt-[72px]">{children}</main>
           <FooterDemo />
         </div>
