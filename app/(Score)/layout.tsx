e3c0489f109e6bbf57625853f23a63ea4a4a49c7
TreNgheDiCode <gabayan170@gmail.com>
2023-11-03 17:00:51 +0700

    Fix Type Errors

--- app/(Score)/layout.tsx
+++ app/(Score)/layout.tsx
@@ -1,7 +1,5 @@
-
 import Navbar from "../../components/navbar/navbar";
 import FooterDemo from "../../components/Footer/footer";
-import { NextUIProvider } from "@nextui-org/react";
 import prismadb from "../../lib/prismadb";
 
 const LienHeLayout = async ({
@@ -15,15 +13,13 @@ const LienHeLayout = async ({
     },
   });
   return (
-    <NextUIProvider>
-      <div className="h-full flex flex-col">
-        <div className="flex-1">
-          <Navbar schools={schools} />
-          <main className="w-full pt-[72px]">{children}</main>
-          <FooterDemo />
-        </div>
+    <div className="h-full flex flex-col">
+      <div className="flex-1">
+        <Navbar schools={schools} />
+        <main className="w-full pt-[72px]">{children}</main>
+        <FooterDemo />
       </div>
-    </NextUIProvider>
+    </div>
   );
 };
 
