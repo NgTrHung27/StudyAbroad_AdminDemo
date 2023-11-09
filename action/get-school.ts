3cf214c6db7256076e23fd09ba7f45e264ad064d
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-11-09 23:25:54 +0700

    huong code lại nganh hoc

--- action/get-school.ts
+++ action/get-school.ts
@@ -4,7 +4,6 @@ const URL = `${process.env.NEXT_PUBLIC_ADMIN_URL}/schools`;
 
 const getSchool = async (nameTruong: string): Promise<School> => {
   const res = await fetch(`${URL}/${nameTruong}`);
-
   return res.json();
 };
 
--- action/get-schools.ts
+++ action/get-schools.ts
@@ -1,11 +1,7 @@
 import { School } from "@prisma/client";
-
 const URL = `${process.env.NEXT_PUBLIC_ADMIN_URL}/schools`;
-
 const getSchools = async (): Promise<School[]> => {
   const res = await fetch(URL);
-
   return res.json();
 };
-
 export default getSchools;
