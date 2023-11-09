c733627ed03e90efef3e13a83c9ae158fd40ae1c
TreNgheDiCode <gabayan170@gmail.com>
2023-11-09 23:15:46 +0700

    Prefix Get Schools

--- action/get-schools.ts
+++ action/get-schools.ts
@@ -3,7 +3,11 @@ import { School } from "@prisma/client";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/schools`;
 
 const getSchools = async (): Promise<School[]> => {
-  const res = await fetch(URL, {next: {tags: ["school"]}});
+  const res = await fetch(URL);
+
+  if (!res.ok) {
+    throw new Error("Lấy trường học thất bại");
+  }
 
   return await res.json();
 };
