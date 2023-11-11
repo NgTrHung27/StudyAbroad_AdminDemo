94d19ff238d7f64ea6df0290a4b0c9b7bf027f69
felixquang <tranquang071203@gmail.com>
2023-11-11 15:42:51 +0700

    Quang moi chay dc admin roi

--- app/(home)/News_Detail/loading.tsx
+++ app/(home)/News_Detail/loading.tsx
@@ -1,3 +1,13 @@
-export default function Loading() {
-  return "Loading...";
+"use client";
+
+import { Loader } from "../../../components/loader";
+
+const Loading = () => {
+  return ( 
+    <div className="flex h-full w-full items-center justify-center">
+      <Loader />
+    </div>
+  );
 }
+
+export default Loading;
