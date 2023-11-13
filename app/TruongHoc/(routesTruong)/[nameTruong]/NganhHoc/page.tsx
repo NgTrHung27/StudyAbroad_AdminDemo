6cd8436a72e30824ca11ddf00c06ce14fe5ef544
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2023-11-13 16:22:20 +0700

    bosung nav

--- app/TruongHoc/(routesTruong)/[nameTruong]/NganhHoc/page.tsx
+++ app/TruongHoc/(routesTruong)/[nameTruong]/NganhHoc/page.tsx
@@ -3,13 +3,18 @@ import { Header } from "./components/header";
 import { Body } from "./components/body";
 import { Body2 } from "./components/body2";
 import getPrograms from "../../../../../action/get-programs";
+import NavbarTruong from "../../../../../components/navbar/navbar";
+import getSchool from "../../../../../action/get-school";
 
 export const revalidate = 0;
 
 const NganhHoc = async ({ params }: { params: { nameTruong: string } }) => {
   const programs = await getPrograms(params.nameTruong);
+  const school = await getSchool(params.nameTruong);
+
   return (
     <div className="">
+     <NavbarTruong school={school} />
       <Header />
       <Body programs={programs} />
       <Body2 />
