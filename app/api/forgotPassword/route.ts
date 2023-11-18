bf73627de1fde5e7683ca9bcdd61b8c7f1b6697e
TreNgheDiCode <gabayan170@gmail.com>
2023-11-18 13:05:44 +0700

    Forgot Password API Routes

---
+++ app/api/forgotPassword/route.ts
@@ -0,0 +1,105 @@
+import db from "@/lib/db";
+import { sendMail } from "@/service/mailService";
+import forgotPassword from "@/templates/forgotPasswordTemplate";
+import { NextResponse } from "next/server";
+
+const otpGenerator = require("otp-generator");
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email } = body;
+
+    if (!email) {
+      return new NextResponse("Không tìm thấy email", { status: 404 });
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        email,
+        emailVerified: {
+          not: undefined,
+        },
+      },
+    });
+
+    if (!user) {
+      return new NextResponse(
+        "Không tìm thấy tài khoản với email này hoặc chưa xác thực email",
+        {
+          status: 404,
+          headers: corsHeaders,
+        }
+      );
+    }
+
+    const existOtp = await db.otp.findFirst({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    let otp;
+
+    if (!existOtp) {
+      const otpString = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        specialChars: false,
+      });
+
+      const expiresAt = Date.now() + 60 * 1;
+
+      otp = await db.otp.create({
+        data: {
+          userId: user.id,
+          otp: otpString,
+          expiresAt: new Date(expiresAt),
+        },
+      });
+    } else {
+      const otpString = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        specialChars: false,
+      });
+
+      const expiresAt = Date.now() + 60 * 1;
+
+      otp = await db.otp.update({
+        where: {
+          userId: user.id,
+        },
+        data: { otp: otpString, expiresAt: new Date(expiresAt) },
+      });
+    }
+
+    const emailTemplate = forgotPassword(user.id, otp.otp);
+
+    const options = {
+      to: email,
+      subject: "[QUẢN LÝ DU HỌC SINH CANADA] - Yêu cầu khôi phục lại mật khẩu",
+      text: emailTemplate.text,
+      html: emailTemplate.html,
+    };
+
+    await sendMail({ options });
+
+    return new NextResponse("Gửi email khôi phục mật khẩu thành công", {
+      status: 200,
+      headers: corsHeaders,
+    });
+  } catch (error) {
+    console.log("FORGOT PASS ERROR", error);
+    return new NextResponse("Gửi email khôi phục mật khẩu thất bại");
+  }
+}
---
+++ app/api/otps/[otpString]/[userId]/route.ts
@@ -0,0 +1,35 @@
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { otpString: string; userId: string } }
+) {
+  try {
+    if (!params.otpString) {
+      return new NextResponse("Không tìm thấy mã yêu cầu", { status: 404 });
+    }
+
+    if (!params.userId) {
+      return new NextResponse("Không tìm thấy mã người dùng", { status: 404 });
+    }
+
+    const otp = await db.otp.findUnique({
+      where: {
+        otp: params.otpString,
+        userId: params.userId,
+      },
+    });
+
+    if (!otp) {
+      return new NextResponse("Không tìm thấy yêu cầu", { status: 404 });
+    }
+
+    return NextResponse.json(otp);
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Lỗi tìm kiếm yêu cầu khôi phục mật khẩu", {
+      status: 500,
+    });
+  }
+}
