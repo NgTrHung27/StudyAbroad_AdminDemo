223606444cd55e4e6c63262f4da9a96cb98fa6bd
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-11-18 14:45:27 +0700

    huong sua lai post

--- app/api/schools/[nameTruong]/programs/route.ts
+++ app/api/schools/[nameTruong]/programs/route.ts
@@ -77,46 +77,4 @@ export async function POST(
   }
 }
 
-export async function POST(
-  req: Request,
-  { params }: { params: { nameTruong: string } }
-) {
-  try {
-    const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-      return new NextResponse("Chưa xác thực", { status: 404 });
-    }
-
-    if (!params.nameTruong) {
-      return new NextResponse("Không tìm thấy tên trường", { status: 404 });
-    }
-
-    const body = await req.json();
-    const { ...values } = formCreateProgramSchema.parse(body);
-
-    const school = await db.school.findUnique({
-      where: {
-        name: params.nameTruong,
-      },
-    });
-
-    if (!school) {
-      return new NextResponse("Không tìm thấy trường học", { status: 404 });
-    }
-
-    const program = await db.program.create({
-      data: {
-        schoolId: school.id,
-        ...values,
-      },
-    });
-
-    return NextResponse.json(program);
-  } catch (error) {
-    console.log("CREATE PROGRAM", error);
-    return new NextResponse(`Tạo ngành học thất bại ${error}`, {
-      status: 500,
-    });
-  }
-}
