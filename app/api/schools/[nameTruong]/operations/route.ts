2904d40e4efe6968607a5e72ad5ffd0e8b5b2083
Huongcute <117435627+Huongcutee@users.noreply.github.com>
2023-11-18 19:00:20 +0700

    operation api

--- app/api/schools/[nameTruong]/operations/route.ts
+++ app/api/schools/[nameTruong]/operations/route.ts
@@ -3,6 +3,38 @@ import { formCreateOperationSchema } from "@/constants/create-opeartion-schema";
 import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { nameTruong: string } }
+) {
+  try {
+    if (!params.nameTruong) {
+      return new NextResponse("Không tìm thấy tên trường", { status: 404 });
+    }
+
+    const school = await db.school.findUnique({
+      where: {
+        name: params.nameTruong,
+      },
+    });
+
+    if (!school) {
+      return new NextResponse("Không tìm thấy trường học", { status: 404 });
+    }
+
+    const opeartion = await db.operation.findMany({
+      where: {
+        schoolId: school.id,
+      },
+    });
+
+    return NextResponse.json(opeartion);
+   
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("CREATE OPERATION ERROR", { status: 500 });
+  }
+}
 export async function POST(
   req: Request,
   { params }: { params: { nameTruong: string } }
@@ -31,16 +63,19 @@ export async function POST(
       return new NextResponse("Không tìm thấy trường học", { status: 404 });
     }
 
-    const operation = await db.operation.create({
+    const opeartion = await db.operation.create({
       data: {
         schoolId: school.id,
         ...values,
       },
     });
-
-    return NextResponse.json(operation);
-  } catch (error) {
-    console.log(error);
-    return new NextResponse("CREATE OPERATION ERROR", { status: 500 });
+    return NextResponse.json(opeartion);
+  } 
+  catch (error) {
+    console.log("CREATE PROGRAM", error);
+    return new NextResponse(`Tạo ngành học thất bại ${error}`, 
+    {
+      status: 500,
+    });
   }
 }
