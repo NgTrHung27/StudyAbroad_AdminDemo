5b981d43cfeea8e24b68fdb2b6b039cc32d2fc0c
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2023-11-20 12:10:12 +0700

    Navbar full

--- app/TruongHoc/(routesTruong)/[nameTruong]/NganhHoc/[nameNganh]/page.tsx
+++ app/TruongHoc/(routesTruong)/[nameTruong]/NganhHoc/[nameNganh]/page.tsx
@@ -5,13 +5,16 @@ import { Body2 } from "./components/body2";
 import getPrograms from "../../../../../../action/get-programs";
 import getSchool from "../../../../../../action/get-school";
 import NavbarTruong from "../../../../../../components/navbar/navbar";
+import getSchools from "../../../../../../action/get-schools";
 export const revalidate = 0;
 const NganhHoc = async ({ params }: { params: { nameTruong: string } }) => {
   const programs = await getPrograms(params.nameTruong);
   const school = await getSchool(params.nameTruong);
+  const schools = await getSchools();
+
   return (
     <div className="">
-      <NavbarTruong school={school} />
+      <NavbarTruong school={school} schools={schools}/>
       <Header school={school} />
       <Body programs={programs} />
       <Body2 />
--- components/navbar/nav-item.tsx
+++ components/navbar/nav-item.tsx
@@ -2,18 +2,44 @@
 import { School } from "@prisma/client";
 import Link from "next/link";
 import React from "react";
+import { TruongHocCombobox } from "../ui/truonghoc-combobox";
 
 interface Props {
   school: School;
+  schools: School[];
 }
-const NavItem = ({ school }: Props) => {
+const NavItem = ({ school, schools }: Props) => {
+  console.log(school);
   return (
     <div className="hidden md:flex flex-row items-center justify-center mx-auto gap-3 text-center md:h-[72px] md:text-lg text-base">
-      <Link className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8" href={"/"}>Trang chủ</Link>
-      <Link className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8" href={"/"}>Trường Học</Link>
-      <Link className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8" href={`/TruongHoc/${school?.name}/YeuCau`}>Yêu Cầu</Link>
-      <Link className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8" href={"/TruongHoc/id/DoiTac"}> Đối Tác</Link>
-      <Link className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8" href={`/TruongHoc/${school?.name}/NganhHoc/nameNganh`}>Ngành Học</Link>
+      <Link
+        className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8"
+        href={"/"}
+      >
+        Trang chủ
+      </Link>
+      <div className="flex flex-row items-center justify-center">
+        <TruongHocCombobox schools={schools} />
+      </div>
+      <Link
+        className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8"
+        href={`/TruongHoc/${school?.name}/YeuCau`}
+      >
+        Yêu Cầu
+      </Link>
+      <Link
+        className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8"
+        href={`/TruongHoc/${school?.name}/DoiTac`}
+      >
+        {" "}
+        Đối Tác
+      </Link>
+      <Link
+        className="text-white font-semibold transition hover:cursor-pointer hover:bg-white hover:text-red-900 min-h-full flex justify-center text-center items-center px-8"
+        href={`/TruongHoc/${school?.name}/NganhHoc/nameNganh`}
+      >
+        Ngành Học
+      </Link>
     </div>
   );
 };
